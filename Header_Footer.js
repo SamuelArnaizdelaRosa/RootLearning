53a3a3259afc456ce1333030069d9c19bb532d9f
RubenAlvarezCampus <ruben.alvarez@campusfp.es>
2022-10-20 23:40:13 +0200

    guardado 2

--- Header_Footer.js
+++ Header_Footer.js
@@ -4,14 +4,14 @@ function showBar() {
     boton_salir = document.getElementById("boton_salida");
     boton_buscar = document.getElementById("boton_busqueda");
 
-    if (elem.className=="active") {
+    if (elem.className=="activo") {
         elem.className = "desactivado";
         buscar.className = "busqueda";
         boton_salir.className = "boton_salida--desactivado";
         boton_buscar.className = "boton_busqueda--active";
     }
     else {
-        elem.className = "active";
+        elem.className = "activo";
         buscar.className = "busqueda--active";
         boton_salir.className = "boton_salida--active";
         boton_buscar.className = "boton_busqueda--desactivado";
@@ -36,3 +36,137 @@ function loaded() {
 
     document.getElementById("busqueda_texto_item").innerHTML = busqueda
 };
+
+window.addEventListener("load",function() {
+    new Glider(this.document.querySelector(".carousel_lista"), {
+        slidesToShow: 1.25,
+        slidesToScroll: 1,
+        draggable: true,
+        dots: ".carousel_indicador",
+        arrows: {
+            prev: ".carrousel_anterior",
+            next: ".carrousel_siguiente"
+        },
+        responsive: [
+            {
+              // screens greater than >= 775px
+              breakpoint: 650,
+              settings: {
+                // Set to `auto` and provide item width to adjust to viewport
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                itemWidth: 150,
+                duration: 0.25
+              }
+            },{
+              // screens greater than >= 1024px
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                itemWidth: 150,
+                duration: 0.25
+              }
+            }
+        ]
+    });
+
+    new Glider(this.document.querySelector(".carousel_lista_2"), {
+        slidesToShow: 1.25,
+        slidesToScroll: 1,
+        draggable: true,
+        dots: ".carousel_indicador_2",
+        arrows: {
+            prev: ".carrousel_anterior_2",
+            next: ".carrousel_siguiente_2"
+        },
+        responsive: [
+            {
+              // screens greater than >= 775px
+              breakpoint: 650,
+              settings: {
+                // Set to `auto` and provide item width to adjust to viewport
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                itemWidth: 150,
+                duration: 0.25
+              }
+            },{
+              // screens greater than >= 1024px
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                itemWidth: 150,
+                duration: 0.25
+              }
+            }
+        ]
+    });
+
+    new Glider(this.document.querySelector(".carousel_lista_3"), {
+        slidesToShow: 1.25,
+        slidesToScroll: 1,
+        draggable: true,
+        dots: ".carousel_indicador_3",
+        arrows: {
+            prev: ".carrousel_anterior_3",
+            next: ".carrousel_siguiente_3"
+        },
+        responsive: [
+            {
+              // screens greater than >= 775px
+              breakpoint: 650,
+              settings: {
+                // Set to `auto` and provide item width to adjust to viewport
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                itemWidth: 150,
+                duration: 0.25
+              }
+            },{
+              // screens greater than >= 1024px
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                itemWidth: 150,
+                duration: 0.25
+              }
+            }
+        ]
+    });
+
+    new Glider(this.document.querySelector(".carousel_lista_4"), {
+        slidesToShow: 1.25,
+        slidesToScroll: 1,
+        draggable: true,
+        dots: ".carousel_indicador_4",
+        arrows: {
+            prev: ".carrousel_anterior_4",
+            next: ".carrousel_siguiente_4"
+        },
+        responsive: [
+            {
+              // screens greater than >= 775px
+              breakpoint: 650,
+              settings: {
+                // Set to `auto` and provide item width to adjust to viewport
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                itemWidth: 150,
+                duration: 0.25
+              }
+            },{
+              // screens greater than >= 1024px
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                itemWidth: 150,
+                duration: 0.25
+              }
+            }
+        ]
+    });
+});
